4ac999808f2586efddc330a9c4d981f071ddcbeb
caherfe <caherfe@hotmail.com>
2013-11-21 14:05:57 +0100

    prueba de nuevo

--- Arkanoid/src/arkanoid/Arkanoid.java
+++ Arkanoid/src/arkanoid/Arkanoid.java
@@ -17,7 +17,8 @@ public class Arkanoid {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        /*HOla Carmen*/
+        /*HOla Carmen
+        Hola de nuevo jose*/
     }
     
 }
