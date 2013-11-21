821f9a9a9074c6ea675959526ff9005f27c6c932
jvcerver <jv.cervero.oller@gmail.com>
2013-11-21 11:40:09 +0100

    incluida clase Recursos

---
+++ Arkanoid/src/arkanoid/Recursos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arkanoid;
+
+import FRAMEWORK.GRAFICOS.BitMap;
+
+/**
+ *
+ * @author josevicente
+ */
+public class Recursos {
+    public static BitMap barra = new BitMap("bar.jpg");
+}
